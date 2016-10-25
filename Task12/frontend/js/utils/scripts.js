faf36a76c1fee8c99e9dd6efc98bc85e6624e4c9
A. Gutriz <a.gutriz@initlab.ru>
2016-10-25 16:57:42 +0200

    Task 12 25.10

--- Task12/frontend/js/utils/scripts.js
+++ Task12/frontend/js/utils/scripts.js
@@ -1,7 +1,7 @@
 Date.prototype.formatDate = function () {
   var options = {
     year: "numeric", month: "numeric",
-    day: "numeric", hour: "2-digit", minute: "2-digit"
+    day: "numeric", hour: "2-digit", minute: "2-digit", hour12: false
   };
 
   return this.toLocaleTimeString("en-us", options);
